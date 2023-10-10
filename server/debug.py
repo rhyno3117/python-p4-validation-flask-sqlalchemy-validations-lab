1ba4bc17084c6b57d082730829a5a5b9e7e0c0b9
Ryan Meyers <rhynogolfer42@gmail.com>
2023-10-09 21:47:30 -0600

    Adds myfile.md

--- server/debug.py
+++ server/debug.py
@@ -8,4 +8,11 @@ from models import db, Author, Post
 if __name__ == '__main__':
     
     with app.app_context():
+        # a1 = Author(name="Baran", phone_number = "00000")
+        # db.session.add(a1)
+        # db.session.commit()
+        # a1 = Author(name="Tri", phone_number = "3032234567")
+        # db.session.add(a2)
+        # db.session.commit()
+         
         import ipdb; ipdb.set_trace()
--- server/models.py
+++ server/models.py
@@ -4,7 +4,6 @@ db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
@@ -12,12 +11,27 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    # Add validations and constraints 
+    @validates('name')
+    def validate_name(self, key, name):
+        names = [item[0] for item in db.session.query(Author.name).all()]
+        if not name:
+            raise ValueError('Name is required')
+        elif name in names:
+            raise ValueError('Name must be unique')
+        return name
+    
+    @validates("phone_number")
+    def validates_phone_number(self, key, number):
+        if len(number) != 10:
+            raise ValueError('Number has to be 10 digits!')
+        return number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -27,6 +41,28 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    # Add validations and constraints 
+    @validates("title")
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait)
+            raise ValueError("Not clickbait enough!")
+        return title
+    
+    @validates("cpntent", "summary")
+    def validates_content_summary_length(self,key,string):
+        if key == "content" and len(string) < 250:
+                raise ValueError("Content must be longer!")
+        if key == "summary" and len(string) > 250:
+                raise ValueError("Summary must be shorter!")
+        return string
+    
+    @validates("category")
+    def validates_category(self,key,category):
+         ALLOWED_CATEGORIES = ["Fiction", "Non-Fication"]
+         if category not in ALLOWED_CATEGORIES:
+              raise ValueError("Bad category")
+         return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
